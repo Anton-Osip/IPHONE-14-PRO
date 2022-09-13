c4d92662096ec7fca74da68121065d3574f3b461
Anton Osipchyk <osipchika@gmail.com>
2022-09-13 11:41:03 +0300

    refactoring

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 const menuBtn = document.querySelector('.menu-btn')
 const nav = document.querySelector('.nav')
+const menuItems = document.querySelectorAll('.menu-item')
 
+menuItems.forEach(item =>
+	item.addEventListener('click', () => {
+		nav.classList.remove('menu-open')
+	}),
+)
 menuBtn.addEventListener('click', () => {
 	nav.classList.toggle('menu-open')
 })
